39becbb2ce50008a50ab04f14bbb43f289217c55
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2021-02-20 18:55:00 +0100

    feat: compute number and size of color squares in the final picture

--- TextImage.py
+++ TextImage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3*
+from math import sqrt
 from Text import Text
 
 class TextImage:
@@ -11,3 +12,14 @@ class TextImage:
         
     def save(self, imagePath, extension=".png", dimX="2000", dimY="2000"):
         pass
+    
+    def draw(self, dimX, dimY):
+        pass            
+    
+    def __getSquareSideLength(self, gridDimX, gridDimY):
+        totalArea = gridDimX * gridDimY
+        squareArea = totalArea / len(self.text.color) # Single square area (px) = totalArea (px) / number of colors
+        return sqrt(squareArea)
+    
+    def __getNumberOfSquaresPerRow(self, gridDimX, gridDimY):
+        return gridDimX / self.__getSquareSideLength(gridDimX, gridDimY)
