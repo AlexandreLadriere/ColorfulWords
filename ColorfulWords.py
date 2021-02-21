2b7a2e698f4bd7c7a272a40880255a17f9416273
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2021-02-21 17:22:31 +0100

    feat: Draw text color block

--- ColorfulWords.py
+++ ColorfulWords.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3*
 from Text import Text
+from TextImage import TextImage
 
 def main():
-    textObj = Text(text="Le : petit Prince est le deuxième livre le plus traduit au monde.")
+    textObj = Text(text="Le : petit Prince est le deuxième livre le plus traduit au monde Alexnadre.")
     print(textObj.colors)
+    textImg = TextImage(textObj)
+    print(textImg.text.colors)
+    textImg.draw(1000)
 
 if __name__ == '__main__':
     main()
--- TextImage.py
+++ TextImage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3*
+from PIL import Image, ImageDraw
 from math import sqrt
 from Text import Text
 
@@ -10,16 +11,36 @@ class TextImage:
         self.text = text
         
         
-    def save(self, imagePath, extension=".png", dimX="2000", dimY="2000"):
+    def save(self, imagePath, extension=".png", dimX="2000"):
         pass
     
-    def draw(self, dimX, dimY):
-        pass            
+    def draw(self, dimX):
+        img = Image.new('RGB', (dimX, dimX), color='white')
+        draw = ImageDraw.Draw(img)
+        squareNumX = 0
+        squareNumY = 0
+        squareSize = self.__getSquareSideLength(dimX)
+        borderRatio = squareSize / 10
+        for color in self.text.colors:
+            upperLeftX = squareNumX * squareSize + borderRatio
+            upperLeftY = squareNumY * squareSize + borderRatio
+            lowerRightX = (squareNumX + 1) * squareSize - borderRatio
+            lowerRightY = (squareNumY + 1) * squareSize - borderRatio
+            draw.rectangle((upperLeftX, upperLeftY, lowerRightX, lowerRightY), fill=(color[0], color[1], color[2]))
+            squareNumX += 1
+            if squareNumX == self.__getGridSize():
+                squareNumX = 0
+                squareNumY += 1
+        img.save("./test.png")
+            
     
-    def __getSquareSideLength(self, gridDimX, gridDimY):
-        totalArea = gridDimX * gridDimY
-        squareArea = totalArea / len(self.text.color) # Single square area (px) = totalArea (px) / number of colors
-        return sqrt(squareArea)
-    
-    def __getNumberOfSquaresPerRow(self, gridDimX, gridDimY):
-        return gridDimX / self.__getSquareSideLength(gridDimX, gridDimY)
+    def __getGridSize(self):
+        squareRoot = sqrt(len(self.text.colors))
+        if squareRoot % 1 == 0:
+            return squareRoot
+        else:
+            return int(squareRoot) + 1
+        
+    def __getSquareSideLength(self, dimX):
+        return dimX / self.__getGridSize()
+        
