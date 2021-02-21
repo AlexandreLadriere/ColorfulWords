a248dda30cd1db679fc2718a6cd5d0a9baba7fc7
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2021-02-21 17:37:28 +0100

    fix: changing text used for test

--- ColorfulWords.py
+++ ColorfulWords.py
@@ -3,11 +3,11 @@ from Text import Text
 from TextImage import TextImage
 
 def main():
-    textObj = Text(text="Le : petit Prince est le deuxième livre le plus traduit au monde Alexnadre.")
+    textObj = Text(text="Le petit prince est le deuxième livre le plus traduit au monde, après la Bible.")
     print(textObj.colors)
     textImg = TextImage(textObj)
     print(textImg.text.colors)
-    textImg.draw(1000)
+    textImg.save("./test")
 
 if __name__ == '__main__':
     main()
